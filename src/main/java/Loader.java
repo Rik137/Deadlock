40ae3c4d26411e85a3cc1a3777e204ead8f1ff46
Sergey Nazarov <ric19902002@icloud.com>
2025-09-23 08:41:19 +0000

    Update Loader.java

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -1,8 +1,8 @@
 public class Loader {
 
     public static void main(String[] args) {
-        final Friend vasya = new Friend("Вася");
-        final Friend vitya = new Friend("Витя");
+        final Friend vasya = new Friend("Rick");
+        final Friend vitya = new Friend("Marina");
 
         new Thread(() -> vasya.throwBallTo(vitya)).start();
         new Thread(() -> vitya.throwBallTo(vasya)).start();
